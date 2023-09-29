46d42773c8b1b93b3cf4dfb06254bdde389bcb82
Yuliya Nureeva <julianureevs@gmail.com>
2023-09-29 19:09:58 +0300

    Add is timer program

---
+++ timer.pas
@@ -0,0 +1,7 @@
+﻿begin
+var hours := ReadInteger('введите количество часов');
+var min := ReadInteger('введите количество минут');
+var sec := ReadInteger('введите количество секунд');
+var time := hours*60*60 + min*60 + sec;
+print(time);
+end.
