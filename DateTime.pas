ccfda6c84acee51f0ade0ea9856f8d46375ec806
Yuliya Nureeva <julianureevs@gmail.com>
2023-09-29 19:04:04 +0300

    Add is new leap year program

--- DateTime.pas
+++ DateTime.pas
@@ -1,7 +1,7 @@
 ﻿begin
-var year := ReadInteger('');
+var year := ReadInteger('введите год');
 Assert(year>0);
-if year mod 4 = 0 then print('True');
-if year mod 4 <> 0 then print('False');
+if (year mod 4 = 0) and ((year mod 100 <> 0) or (year mod 400 = 0)) then print('366 дней');
+if year mod 4 <> 0 then print('365 дней');
 
 end.
