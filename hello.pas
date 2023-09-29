e9342f08af6246ae557e7745942769c6be3ee98e
Yuliya Nureeva <julianureevs@gmail.com>
2023-09-29 18:48:33 +0300

    Add is hello program

---
+++ hello.pas
@@ -0,0 +1,10 @@
+﻿begin
+var hour := ReadInteger('введите час');
+Assert((hour>=0) and (hour<24));
+case hour of
+4..10: print('Доброе утро, мир!');
+11..16: print('Добрый день, мир!');
+17..22: print('Добрый вечер, мир!');
+else print('Доброй ночи, мир!');
+end;
+end.
